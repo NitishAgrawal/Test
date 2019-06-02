aca98991022c2934f9c0c7d8ef7cf7557b701474
Nitish Agrawal <agrawal.nitish3008@gmail.com>
2019-06-02 23:46:55 +0530

    Update row color

--- app/screens/AllStoresScreen/index.js
+++ app/screens/AllStoresScreen/index.js
@@ -32,6 +32,7 @@ const styles = StyleSheet.create({
     borderColor: 'gray',
     width: width - 32,
     borderRadius: 5,
+    backgroundColor: '#00000022',
   },
   bodyTitle: {
     fontSize: 18,
